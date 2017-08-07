4f4ba134fa1a6a18ca874c872ec0e17fa7f8d9e2
Reinharderino <reinaldocelis@outlook.com>
2017-08-06 20:22:03 -0400

    SPA primer episodio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/components/home/home.component';
+import { AboutComponent } from './components/components/about/about.component';
+import { HeroesComponent } from './components/components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
